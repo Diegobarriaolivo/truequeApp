d797420dda4d75952f94ba06c4ac5225038c533d
diegobarria <diegobarria22@gmail.com>
2022-11-24 18:51:13 -0300

    se agrego el propyecto

---
+++ truequeApp/Articulo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace truequeApp
+
+    internal class Articulo
+    {
+        int codigo;
+        string nombre;
+
+
+        public Articulo(int codigo, string nombre)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+        }
+
+        public int getCodigo()
+        {
+            return this.codigo;
+        }
+
+        private void setCodigo(int codigo)
+        {
+            this.codigo = codigo;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        private void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Articulo articulo &&
+                   codigo == articulo.codigo &&
+                   nombre == articulo.nombre &&
+                   EqualityComparer<TextWriter>.Default.Equals(archivo, articulo.archivo) &&
+                   mensaje == articulo.mensaje;
+        }
+    }
+}
---
+++ truequeApp/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace truequeApp
+{
+    internal class Usuario
+    {
+        public string rut;
+        public string nombre;
+
+        public List<Articulo> articulos = new List<Articulo>();
+        public Usuario(string rut, string nombre)
+        {
+            this.rut = rut;
+            this.nombre = nombre;
+        }
+
+        public string getRut()
+        {
+            return this.rut;
+        }
+
+        private void setRut(string rut)
+        {
+            this.rut = rut;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        private void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
